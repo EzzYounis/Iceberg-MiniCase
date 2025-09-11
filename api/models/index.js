7381c86abcb38b10ec0f1835d258a0c604754cc2
EZZALDEEN E A YOUNIS <ezzyounis1@gmail.com>
2025-09-11 12:26:17 +0300

    Models Associations

---
+++ api/models/index.js
@@ -0,0 +1,46 @@
+// Centralizes and initializes all models and their associations
+
+const Party = require('./Party');
+const Property = require('./Property');
+const Transaction = require('./Transaction');
+const TransactionParty = require('./TransactionParty');
+const Payment = require('./Payment');
+const Document = require('./Document');
+
+// Associations
+// Property 1<-->N Transaction
+Property.hasMany(Transaction, { foreignKey: 'property_id' });
+Transaction.belongsTo(Property, { foreignKey: 'property_id' });
+
+// Transaction 1<-->N TransactionParty
+Transaction.hasMany(TransactionParty, { foreignKey: 'transaction_id' });
+TransactionParty.belongsTo(Transaction, { foreignKey: 'transaction_id' });
+
+// Party 1<-->N TransactionParty
+Party.hasMany(TransactionParty, { foreignKey: 'party_id' });
+TransactionParty.belongsTo(Party, { foreignKey: 'party_id' });
+
+// Transaction 1<-->N Payment
+Transaction.hasMany(Payment, { foreignKey: 'transaction_id' });
+Payment.belongsTo(Transaction, { foreignKey: 'transaction_id' });
+
+// Party 1<-->N Payment
+Party.hasMany(Payment, { foreignKey: 'party_id' });
+Payment.belongsTo(Party, { foreignKey: 'party_id' });
+
+// Transaction 1<-->N Document
+Transaction.hasMany(Document, { foreignKey: 'transaction_id' });
+Document.belongsTo(Transaction, { foreignKey: 'transaction_id' });
+
+// Party 1<-->N Document (optional)
+Party.hasMany(Document, { foreignKey: 'party_id' });
+Document.belongsTo(Party, { foreignKey: 'party_id' });
+
+module.exports = {
+  Party,
+  Property,
+  Transaction,
+  TransactionParty,
+  Payment,
+  Document,
+};
